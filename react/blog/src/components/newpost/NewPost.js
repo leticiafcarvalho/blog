bc12aa4a3c510e2fb4a41b654b396ea197d85eeb
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2021-01-28 01:49:24 -0300

    form post

--- react/blog/src/components/newpost/NewPost.js
+++ react/blog/src/components/newpost/NewPost.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-const NewPost = ({onPostSubmit}) => {
+const NewPost = ({ onPostSubmit }) => {
     return (
-        <div class="form-group">
+        <div class="form-group container-fluid">
+            <h1>Cadastro de novo Post</h1>
             <form onSubmit={onPostSubmit}>
-                <input type="text" class="form-control" id="titulo" placeholder="Digite um titulo" />
-                <input type="text" class="form-control" id="descricao" placeholder="Descrição" />
-                <input type="text" class="form-control" id="localizacao" placeholder="Localização" />
-                <input type="text" class="form-control" id="idusuario" placeholder="Id do usuario" />
-                <input type="submit" value="Cadastrar" />
+                <div class="form-group">
+                    <input type="text" class="form-control" id="titulo" placeholder="Digite um titulo" />
+                </div>
+                <div class="form-group">
+
+                    <textarea class="form-control" id="descricao" placeholder="Descrição" />
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="localizacao" placeholder="Localização" />
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="idusuario" placeholder="Id do usuario" />
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Cadastrar" />
+                </div>
+
             </form>
         </div>
     );
