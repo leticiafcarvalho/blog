0ef3f7a315e7ec5a582d092b4dd6d3fa54282ae1
mathdavy <matheusuzumaki26@gmail.com>
2021-01-28 00:03:08 -0300

    Criacao do post de usuarios

--- node/app.js
+++ node/app.js
@@ -1,8 +1,8 @@
 let app = require('./config/server');
 const rotas = require('./app/routes/routes');
 
-rotas.rotaUsuarioSalvar(app);
 rotas.rotaListarUsuarios(app);
+rotas.rotaUsuarioSalvar(app);
 rotas.rotaListarPosts(app);
 rotas.rotaPostSalvar(app);
 rotas.rotaListarPostsPorIdUsuario(app);
--- react/blog/src/components/header/Header.js
+++ react/blog/src/components/header/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
                     <li><NavLink to="/livros/novolivro" exact>Novo</NavLink></li>
                     <li><NavLink to="/livros/alterarlivro" exact>Alterar</NavLink></li>
                     <li><NavLink to="/usuarios" exact>Users</NavLink></li>
+                    <li><NavLink to="/usuarios/salvar" exact>Novo usuario</NavLink></li>
                 </ul>
             </nav>
         </header>
