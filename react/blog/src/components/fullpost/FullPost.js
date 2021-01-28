980f8dabf269ce43781eb414d2a90f95bd6c0bc2
mathdavy <matheusuzumaki26@gmail.com>
2021-01-28 17:27:01 -0300

    Layout do fullpost

--- react/blog/src/components/fullpost/FullPost.js
+++ react/blog/src/components/fullpost/FullPost.js
@@ -1,15 +1,13 @@
-import React, { useEffect } from 'react';
-import Post from '../post/Post';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
+import '../defaultcss/default.css';
 
 const FullPost = () => {
 
     let post = useLocation().state.post;
-    let index = useLocation().state.index;
 
     return (
-        <div className="FullPost">
+        <div className="Default">
             <div>
                 <p>Titulo: {post.titulo}</p>
             </div>
--- react/blog/src/components/post/Post.js
+++ react/blog/src/components/post/Post.js
@@ -6,7 +6,7 @@ const Post = (props) => {
     <div className="Default">
       <br/>
         <p>Título: {props.titulo}</p>
-        <p>Drscrição: {props.descricao}</p>
+        <p>Descrição: {props.descricao}</p>
         <p>Localização: {props.localizacao}</p>
         <button class="btn btn-danger" onClick={props.click}>Apagar post</button>
     </div>
